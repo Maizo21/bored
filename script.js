ed5699315f7a61a2670db5ef9f805c6c762b34db
Hernan Amaiz <66435038+Maizo21@users.noreply.github.com>
2022-03-26 12:32:31 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+let buttons = document.querySelectorAll("button");
+
+const getActivity = (e) => {
+  console.log(e.target.id);
+  let query =
+    e.target.id == "random"
+      ? `http://www.boredapi.com/api/activity/`
+      : `http://www.boredapi.com/api/activity?type=${e.target.id}`;
+
+  fetch(query)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector(".activity").innerHTML = data.activity;
+      document.querySelector(".type").innerHTML = `Type: ${data.type}`;
+      document.querySelector(
+        ".participants"
+      ).innerHTML = `Participants (minimum): ${data.participants}`;
+      document.querySelector(".result").style.opacity = "1";
+    });
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", getActivity);
+});
